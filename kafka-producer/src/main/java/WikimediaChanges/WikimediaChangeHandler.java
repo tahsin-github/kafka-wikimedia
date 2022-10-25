738eb72109d80f779997df6c899a225abac45ebb
Tahsin <tahsin.anam@tietoevry.com>
2022-10-25 12:33:17 +0300

    producer event handler incorporated

---
+++ kafka-producer/src/main/java/WikimediaChanges/WikimediaChangeHandler.java
@@ -0,0 +1,52 @@
+package WikimediaChanges;
+
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getName());
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error in stream reading " + throwable);
+
+    }
+}
--- kafka-producer/src/main/java/WikimediaChanges/WikimediaChangesProducer.java
+++ kafka-producer/src/main/java/WikimediaChanges/WikimediaChangesProducer.java
@@ -5,15 +5,15 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.beans.EventHandler;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         // Read the Kafka Server's ip address from the properties file
         Properties kafkaServerIPProperties = new Properties();
         InputStream is = new FileInputStream("kafka-producer/src/main/java/properties/KafkaServer.properties");
@@ -26,6 +26,10 @@ public class WikimediaChangesProducer {
         KafkaProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        KafkaProperties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "100");
+        KafkaProperties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        KafkaProperties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         // Create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(KafkaProperties);
 
@@ -33,7 +37,7 @@ public class WikimediaChangesProducer {
 
         final String topic = "wikimedia.recentchange";
 
-        EventHandler eventHandler = TODO;
+        WikimediaChangeHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
         EventSource eventSource = builder.build();
@@ -41,5 +45,7 @@ public class WikimediaChangesProducer {
 
         // Start the producer in another thread
         eventSource.start();
+
+        TimeUnit.MINUTES.sleep(1);
     }
 }
