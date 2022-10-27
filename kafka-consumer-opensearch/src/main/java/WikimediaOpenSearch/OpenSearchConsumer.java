afc24220854a2fe55e9c708cb0b0bc083ab5e7b3
Tahsin <tahsin.anam@tietoevry.com>
2022-10-27 14:33:35 +0300

    worked on  the consumer

--- kafka-consumer-opensearch/src/main/java/WikimediaOpenSearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/WikimediaOpenSearch/OpenSearchConsumer.java
@@ -1,4 +1,94 @@
 package WikimediaOpenSearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Properties;
+
 public class OpenSearchConsumer {
+
+    public static RestHighLevelClient createOpenSearchClient() throws IOException {
+
+        // Read the Kafka Server's ip address from the properties file
+        Properties openSearchServer = new Properties();
+        InputStream is = new FileInputStream("kafka-consumer-opensearch/src/main/java/properties/servers.properties");
+        openSearchServer.load(is);
+        String connString = openSearchServer.getProperty("wikimedia.servers");
+
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getName());
+
+        // create an OpenSearch Client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+
+        if(!indexExist){
+            try(openSearchClient){
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("The wikimedia Index has been created");
+            }
+        }
+
+
+
+
+
+
+        // Create Kafka Client
+
+        // Main Code Logic
+
+        // Close things
+    }
 }
