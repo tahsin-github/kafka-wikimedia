e25b3368535055ce5b9ede248a6b61dd895328f4
Tahsin <tahsin.anam@tietoevry.com>
2022-10-28 15:01:04 +0300

    The consumer is updated

--- kafka-consumer-opensearch/src/main/java/WikimediaOpenSearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/WikimediaOpenSearch/OpenSearchConsumer.java
@@ -6,11 +6,19 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +27,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class OpenSearchConsumer {
@@ -70,25 +80,81 @@ public class OpenSearchConsumer {
         // create an OpenSearch Client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
-        boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+        // Create Kafka Client
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+        // Subscribe the consumer to a topic
+        final String topic = "wikimedia.recentchange";
+        consumer.subscribe(Arrays.asList(topic));
 
-        if(!indexExist){
-            try(openSearchClient){
-                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
-                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
-                log.info("The wikimedia Index has been created");
+        try(openSearchClient; consumer){
+            boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+
+
+
+            if(!indexExist){
+                try(openSearchClient){
+                    CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                    openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                    log.info("The wikimedia Index has been created");
+                }
             }
+            else {
+                log.info("The wikimedia Index is already existed.");
+            }
+
+            while (true){
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(2000));
+
+                int recordCount = records.count();
+                log.info("Received " + recordCount + " records.");
+
+                for (ConsumerRecord<String, String> record : records){
+                    try{
+                        // send the data to the opensearch
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(record.value(), XContentType.JSON);
+
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                        log.info("Document inserted : 1 " + response.getId());
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }
+
         }
 
-
-
-
-
-
-        // Create Kafka Client
 
         // Main Code Logic
 
         // Close things
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() throws IOException {
+        final String groupId = "consumer-opensearch.one";
+
+
+        // Read the Kafka Server's ip address from the properties file
+        Properties kafkaServerIPProperties = new Properties();
+        InputStream is = new FileInputStream("kafka-consumer-opensearch/src/main/java/properties/servers.properties");
+        kafkaServerIPProperties.load(is);
+
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerIPProperties.getProperty("bootstrap.servers"));
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+
+        // Create the Consumer
+        return new KafkaConsumer<String, String>(properties);
+
+
     }
 }
